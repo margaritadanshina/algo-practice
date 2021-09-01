f23802396b3846f752450d8e38a2bf40e7b8d30c
Margarita Danshina <margarita.danshina@gmail.com>
2021-09-01 14:19:03 -0400

    merge testing

--- arrays/merge-sorted-array/index.js
+++ arrays/merge-sorted-array/index.js
@@ -4,5 +4,7 @@ function merge(nums1, m, nums2, n) {
     for(let i = 0; i < n; i++) {
         nums1[i + m] = nums2[i]
     }
-    nums1.sort((a,b) => a - b)
+    return nums1.sort((a,b) => a - b)
 };
+
+console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
