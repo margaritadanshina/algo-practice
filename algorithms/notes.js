eba4c913a9e323ce9bb9b3cb435614ec8ecc6bee
Margarita Danshina <margarita.danshina@gmail.com>
2021-10-13 13:40:06 -0400

    hash tables notes

--- algorithms/notes.js
+++ algorithms/notes.js
@@ -10,3 +10,18 @@ function euc(a, b) {
 
 console.log(euc(462, 910))
 
+// Hash table
+// -> is a ds that allows to create a list of paired values
+// it transforms a key into an integer index using a hash function
+//the index will decide where to store key/value pair in memory
+
+
+//Reason to use:
+//fast search, insertion, and delete.
+
+//Space O(n)
+//Search O(1)
+//Insert O(1)
+//Delete O(1)
+
+const ht = new HashTable()
