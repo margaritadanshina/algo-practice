1e125a582a54a90dc4702a1b754e8f01a97bcbaf
Margarita Danshina <margarita.danshina@gmail.com>
2021-11-11 21:38:02 -0500

    meeting rooms

---
+++ easy/meetingRooms.js
@@ -0,0 +1,17 @@
+//Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+function canAttendMeetings(intervals) {
+    if(intervals.length < 1) {
+        return true;
+    }
+    intervals.sort((a, b) => a[0] - b[0]);
+    for(let i = 1; i < intervals.length; i++) {
+        if(intervals[i][0] < intervals[i - 1][1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(canAttendMeetings([0, 30], [5, 10], [15, 20]))
+console.log(canAttendMeetings([7, 10], [2, 4]))
