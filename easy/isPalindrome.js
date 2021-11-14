2841df96e0af4d4dcb7df7487fc63288ed3dae25
Margarita Danshina <margarita.danshina@gmail.com>
2021-11-13 19:39:30 -0500

    isPalidrome recursive

--- easy/isPalindrome.js
+++ easy/isPalindrome.js
@@ -1,12 +1,32 @@
-function isPalindrome(x) {
-    const original = x.toString();
-    const reversed = x.toString().split("").reverse().join("");
-    return original === reversed;
+// function isPalindrome(x) {
+//     const original = x.toString();
+//     const reversed = x.toString().split("").reverse().join("");
+//     return original === reversed;
+// }
+
+
+function isPalindromeRecursive(string) {
+    if(string.length === 1) {
+        return true;
+    }
+    if(string.length === 2) {
+        return string[0] === string[1];
+    }
+    if(string[0] === string.slice(-1)) {
+        return isPalindromeRecursive(string.slice(1, -1));
+    }
+    return false;
 }
 
 
-console.log(isPalindrome(121))
-console.log(isPalindrome(-121))
-console.log(isPalindrome(123))
-console.log(isPalindrome(454))
+console.log(isPalindromeRecursive('awesome'))
+console.log(isPalindromeRecursive('tacocat'))
+console.log(isPalindromeRecursive('foobar'))
+
+
+
+// console.log(isPalindrome(121))
+// console.log(isPalindrome(-121))
+// console.log(isPalindrome(123))
+// console.log(isPalindrome(454))
 
